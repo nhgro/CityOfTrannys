2230ec67fbfe53c565693454ece748e0b42721fc
nhgro <70570241+nhgro@users.noreply.github.com>
2020-11-26 18:17:08 -0500

    Update CityOfTranniesGUIObfuscated.lua

--- CityOfTranniesGUIObfuscated.lua
+++ CityOfTranniesGUIObfuscated.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("You were kicked from this game:  You are banned from this game for:        -Exploiting    Expires"): N/A   Note: This ban cannot be appealed by any circumstances.")
+game.Players.LocalPlayer:Kick("discord.gg/CZ3XheM")
